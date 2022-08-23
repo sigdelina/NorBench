78efd05beaf3eeef251fc1e56f21b1085a35704f
Elina Sigdel <32021201+sigdelina@users.noreply.github.com>
2022-08-24 00:48:04 +0300

    Update data_preparation_sentiment.py

--- NorBench/experiments/data_preparation/data_preparation_sentiment.py
+++ NorBench/experiments/data_preparation/data_preparation_sentiment.py
@@ -15,10 +15,25 @@ class Example:
         self.category_index = category_index
 
 
-def bert_convert_examples_to_tf_dataset(
+def token_type_model_attr(model, max_length):
+    # if model has token_type_ids as an attribute
+    
+    try:
+      input_ids = tf.keras.layers.Input(shape=(max_length,), dtype=tf.int32, name="input_ids")
+      attention_mask=tf.keras.layers.Input(shape=(max_length,), dtype=tf.int32, name="attention_mask")
+      token_type_ids=tf.keras.layers.Input(shape=(max_length,), dtype=tf.int32, name="token_type_ids")
+      model_attr = model({'input_ids': input_ids, 'attention_mask': attention_mask, 'token_type_ids': token_type_ids})
+      return True
+    except:
+      return False
+
+
+def convert_examples_to_tf_dataset(
         examples,
         tokenizer,
+        model,
         max_length=64,
+        token_type_ids_input=True
 ):
     """
     Loads data into a tf.data.Dataset for finetuning a given model.
@@ -26,132 +41,114 @@ def bert_convert_examples_to_tf_dataset(
     Args:
         examples: List of tuples representing the examples to be fed
         tokenizer: Instance of a tokenizer that will tokenize the examples
-        max_length: Maximum string length
+        model: Instance of a transformers model that is needed to check the ability to use token_type_ids
+        max_length: Maximum string length,
+        token_type_ids_input: (bool), parameter to include or exclude token_type_ids
 
     Returns:
         a ``tf.data.Dataset`` containing the condensed features of the provided sentences
     """
     features = []  # -> will hold InputFeatures to be converted later
 
-    for e in examples:
-        # Documentation is really strong for this method, so please take a look at it
-        input_dict = tokenizer.encode_plus(
-            e.text,
-            add_special_tokens=True,
-            max_length=max_length,  # truncates if len(s) > max_length
-            return_token_type_ids=True,
-            return_attention_mask=True,
-            padding="max_length",  # pads to the right by default
-            truncation=True
-        )
-
-        # input ids = token indices in the tokenizer's internal dict
-        # token_type_ids = binary mask identifying different sequences in the model
-        # attention_mask = binary mask indicating the positions of padded tokens
-        # so the model does not attend to them
-
-        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
-                                                     input_dict["token_type_ids"],
-                                                     input_dict['attention_mask'])
-
-        features.append(
-            InputFeatures(
-                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
-                label=e.category_index
-            )
-        )
-
-    def gen():
-        for f in features:
-            yield (
-                {
-                    "input_ids": f.input_ids,
-                    "attention_mask": f.attention_mask,
-                    "token_type_ids": f.token_type_ids,
-                },
-                f.label,
-            )
-
-    return tf.data.Dataset.from_generator(
-        gen,
-        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
-        (
-            {
-                "input_ids": tf.TensorShape([None]),
-                "attention_mask": tf.TensorShape([None]),
-                "token_type_ids": tf.TensorShape([None]),
-            },
-            tf.TensorShape([]),
-        ),
-    )
-
-
-def roberta_convert_examples_to_tf_dataset(
-        examples,
-        tokenizer,
-        max_length=64,
-):
-    """
-    Loads data into a tf.data.Dataset for finetuning a given model.
-
-    Args:
-        examples: List of tuples representing the examples to be fed
-        tokenizer: Instance of a tokenizer that will tokenize the examples
-        max_length: Maximum string length
-
-    Returns:
-        a ``tf.data.Dataset`` containing the condensed features of the provided sentences
-    """
-    features = []  # -> will hold InputFeatures to be converted later
+    token_type_attr = token_type_model_attr(model, max_length)
 
     for e in examples:
-        # Documentation is really strong for this method, so please take a look at it
-        input_dict = tokenizer.encode_plus(
-            e.text,
-            add_special_tokens=True,
-            max_length=max_length,  # truncates if len(s) > max_length
-            return_attention_mask=True,
-            pad_to_max_length=True,  # pads to the right by default
-            truncation=True
-        )
 
-        # input ids = token indices in the tokenizer's internal dict
-        # token_type_ids = binary mask identifying different sequences in the model
-        # attention_mask = binary mask indicating the positions of padded tokens
-        # so the model does not attend to them
-
-        input_ids, attention_mask = (input_dict["input_ids"], input_dict['attention_mask'])
-
-        features.append(
-            InputFeatures(
-                input_ids=input_ids, attention_mask=attention_mask, label=e.category_index
+        if token_type_ids_input == True and token_type_attr == True:
+            # Documentation is really strong for this method, so please take a look at it
+            input_dict = tokenizer.encode_plus(
+                e.text,
+                add_special_tokens=True,
+                max_length=max_length,  # truncates if len(s) > max_length
+                return_token_type_ids=True,
+                return_attention_mask=True,
+                padding="max_length",  # pads to the right by default
+                truncation=True
             )
-        )
+
+            # input ids = token indices in the tokenizer's internal dict
+            # token_type_ids = binary mask identifying different sequences in the model
+            # attention_mask = binary mask indicating the positions of padded tokens
+            # so the model does not attend to them
+
+            input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
+                                                        input_dict["token_type_ids"],
+                                                        input_dict['attention_mask'])
+
+            features.append(
+                InputFeatures(
+                    input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
+                    label=e.category_index
+                )
+            )
+        else:
+            input_dict = tokenizer.encode_plus(
+                e.text,
+                add_special_tokens=True,
+                max_length=max_length,  # truncates if len(s) > max_length
+                return_attention_mask=True,
+                pad_to_max_length=True,  # pads to the right by default
+                truncation=True
+            )
+            
+            input_ids, attention_mask = (input_dict["input_ids"], input_dict['attention_mask'])
+
+            features.append(
+                InputFeatures(
+                    input_ids=input_ids, attention_mask=attention_mask, label=e.category_index
+                )
+            )
 
     def gen():
-        for f in features:
-            yield (
+        
+        if token_type_ids_input == True and token_type_attr == True:
+            for f in features:
+                yield (
+                    {
+                        "input_ids": f.input_ids,
+                        "attention_mask": f.attention_mask,
+                        "token_type_ids": f.token_type_ids,
+                    },
+                    f.label,
+                )
+        else:
+            for f in features:
+                yield (
+                    {
+                        "input_ids": f.input_ids,
+                        "attention_mask": f.attention_mask,
+                    },
+                    f.label,
+                )
+    
+    if token_type_ids_input == True and token_type_attr == True:
+        return tf.data.Dataset.from_generator(
+            gen,
+            ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+            (
                 {
-                    "input_ids": f.input_ids,
-                    "attention_mask": f.attention_mask,
+                    "input_ids": tf.TensorShape([None]),
+                    "attention_mask": tf.TensorShape([None]),
+                    "token_type_ids": tf.TensorShape([None]),
                 },
-                f.label,
-            )
+                tf.TensorShape([]),
+            ),
+        )
+    else:
+        return tf.data.Dataset.from_generator(
+            gen,
+            ({"input_ids": tf.int32, "attention_mask": tf.int32}, tf.int64),
+            (
+                {
+                    "input_ids": tf.TensorShape([None]),
+                    "attention_mask": tf.TensorShape([None]),
+                },
+                tf.TensorShape([]),
+            ),
+        )
 
-    return tf.data.Dataset.from_generator(
-        gen,
-        ({"input_ids": tf.int32, "attention_mask": tf.int32}, tf.int64),
-        (
-            {
-                "input_ids": tf.TensorShape([None]),
-                "attention_mask": tf.TensorShape([None]),
-            },
-            tf.TensorShape([]),
-        ),
-    )
 
-
-def load_dataset(lang_path, tokenizer, max_length, balanced=False,
+def load_dataset(lang_path, tokenizer, model, max_length, balanced=False,
                  dataset_name="test", limit=None):
     logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
     tqdm.pandas(leave=False)
@@ -179,12 +176,20 @@ def load_dataset(lang_path, tokenizer, max_length, balanced=False,
         raise Exception("Must set 'balanced' to True to choose a manual limit.")
 
     # Convert to TF dataset
-    if 'xlm-roberta' in tokenizer.name_or_path:
-        dataset = roberta_convert_examples_to_tf_dataset(
-            [(Example(text=text, category_index=label)) for label, text in df.values], tokenizer,
-            max_length=max_length)
-    else:
-        dataset = bert_convert_examples_to_tf_dataset(
-                [(Example(text=text, category_index=label)) for label, text in df.values], tokenizer,
-                max_length=max_length)
+    token_type_ids_input = False if ('roberta' in tokenizer.name_or_path or 'distilbert' in tokenizer.name_or_path) else True
+
+
+    try:
+        
+        dataset = convert_examples_to_tf_dataset(
+            [(Example(text=text, category_index=label)) for label, text in df.values], tokenizer, model,
+                max_length=max_length, token_type_ids_input=token_type_ids_input)
+        
+
+    except:
+        dataset = convert_examples_to_tf_dataset(
+            [(Example(text=text, category_index=label)) for label, text in df.values], tokenizer, model,
+                max_length=max_length, token_type_ids_input=False)
+
+
     return df, dataset
